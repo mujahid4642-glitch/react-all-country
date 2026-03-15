08c242f4624b946cceefc5fced83b8e90f344755
mujahid4642-glitch <mujahid4642@gamil.com>
2026-03-15 10:33:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Countrys from './compnent/countrys/Countrys'
+
+function App() {
+  
+
+  return (
+    <>
+      
+        <Countrys></Countrys>
+     
+
+   
+
+    
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/compnent/countrys/Countrys.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countrys = () => {
+    return (
+        <div>
+            <h1>all country</h1>
+        </div>
+    );
+};
+
+export default Countrys;
